96908f474f144e502741c98fa39b3a18aba88e22
Nora Söderlund <nora.soderlund@polestar.com>
2023-05-07 21:13:09 +0200

    Updated example

--- src/client/scripts/alert.ts
+++ src/client/scripts/alert.ts
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("Hello!");
-});
+setTimeout(() => {
+    const element = document.createElement("p");
+    element.innerText = "Boo! Are you alert?";
+
+    document.body.append(element);
+}, 5000);
